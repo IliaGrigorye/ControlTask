8aa656fb8b010502b44737355696a7df9db918eb
Ilia Grigoryev <shirokagaricu@mail.ru>
2022-06-14 19:50:08 +0800

    created a program

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array1 = new string[10] {"dog", "cat", "hello", "world", "99999", "qwe", "1", "5467", "king", "Task"};
+string[] array2 = new string[array1.Length];
+
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
+
+//checking the size of array elements
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+//printing an array with elements of the specified size
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
